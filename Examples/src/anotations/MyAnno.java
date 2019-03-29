a75ecebedd08648ef00e2a6ab32f36a91943de43
jliel <kiritobaz@gmail.com>
2019-03-29 17:34:38 -0600

    Annotations

---
+++ Examples/src/anotations/MyAnno.java
@@ -0,0 +1,10 @@
+package anotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@interface MyAnno {
+	String str();
+	int val();
+}
